3a68a6a9350fbea0a225aab81595b3adf33a4ebd
Marius Utheim <marius_utheim@hotmail.com>
2014-11-18 17:48:38 +0100

    DLLs and Main

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GRaff;
+
+namespace Flick
+{
+	class Program
+	{
+		static void Main()
+		{
+			Giraffe.Run();
+		}
+	}
+}
